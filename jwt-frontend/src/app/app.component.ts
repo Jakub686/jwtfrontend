e56f249a76bfd64fb8b006500d7e25a3614169e5
Jakub <jakub.bchyla@gmail.com>
2023-04-25 14:55:20 +0200

    asdf

--- jwt-frontend/src/app/app.component.ts
+++ jwt-frontend/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {SigninComponent} from "./signin/signin.component";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [SigninComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'jwt-frontend';
